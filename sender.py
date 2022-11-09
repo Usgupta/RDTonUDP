54d1470bc49cfcf0d8cd231a65834398913ffb34
usgupta <usgupta@uwaterloo.ca>
2022-11-08 23:49:23 -0500

    untnt

--- sender.py
+++ sender.py
@@ -12,6 +12,8 @@ seqnumlog = "seqnum.log"
 acklog = "ack.log"
 Nlog = "N.log"
 
+timestamp = 0
+
 windowsize = 1
 MAXN = 10
 send_base = 0
@@ -26,7 +28,7 @@ for i in range(3):
 def addlog(file_name,data):
     with open(file_name, mode="a") as fp:
         print("opened {file_name} file")
-        fp.write(str(data) + "\n")
+        fp.write(str(timestamp) + " " + str(data) + "\n")
 
 
 # def convert_int_to_bytes(x):
@@ -78,61 +80,68 @@ while filename != "-1" and (not pathlib.Path(filename).is_file()):
 #     s.sendall(convert_int_to_bytes(2))
 #     break
 
-filename_bytes = bytes(filename, encoding="utf8")
+# filename_bytes = bytes(filename, encoding="utf8")
 
 # Send the filename
 # clientSocket.sendto(filename.encode(),(serverName, serverPort))
 # s.sendall(convert_int_to_bytes(0))
 # s.sendall(convert_int_to_bytes(len(filename_bytes)))
 # clientSocket.sendto(filename_bytes,(emulator_addr, emulator_port))
-
+with open(filename, mode="r") as fp:
+    data = fp.read()
 # Send the file
-def makePackets(filename):
-    seqno = -1
-    packets = []
-    with open(filename, mode="r") as fp:
-        data = fp.read()
-        if len(data)>500:
-            for i in range(0,len(data),500):
-            # print(type(data))
-                ptype = 1
-                seqno+=1
-                seqno=seqno%32
-                lendata = len(data[i:min(i+500,len(data))])
-                print(i)
-                print(data[i:min(i+500,len(data))])
-                packet = Packet(ptype,seqno,lendata,data[i:min(i+500,len(data))])
-                packets.append(packet)
-                
+readptr = 0
+packets = [None] * 32
 
-        else:
-            seqno+=1
-            seqno=seqno%32
-            packet = Packet(1,seqno,len(data),data)
-            packets.append(packets)
-            # clientSocket.sendto(packet.encode(),(emulator_addr, emulator_port))
-            # recvd_packet = clientSocket.recv(1024)
-            # print(recvd_packet)
-    return packets
+def makePackets():
+    if len(data)>500:
+        while(readptr!=len(data)):
+            if(len(packets)==32 and packets.count(None) == 0):
+                break
+            seqno = packet.index(None)
+            ptype = 1
+            lendata = len(data[readptr:min(i+500,len(data))])
+            print(readptr)
+            print(data[i:min(readptr+500,len(data))])
+            packet = Packet(ptype,seqno,lendata,data[readptr:min(readptr+500,len(data))])
+            packets[seqno] = packet
+            readptr+=500
 
+    else:
+        packets[0] = packet(1,0,len(data),data)
 
 def sendPackets(packets):
-    if nextseqnum-send_base-1!=windowsize:
+    if nextseqnum-send_base!=windowsize:
         clientSocket.sendto(packets[nextseqnum].encode(),(emulator_addr, emulator_port))
+        timestamp+=1
         addlog(seqnumlog,packets[nextseqnum].seqnum) #add seq num to seq log
         nextseqnum+=1
-
-
+        nextseqnum=nextseqnum%32
+    # if dup:
+        
 filepackets = makePackets(filename)
-
+dupcount = 0
 def recAck(packets):
     recvd_packet = Packet(clientSocket.recv(1024))
+    timestamp+=1
     print(recvd_packet)
-    for i in range(len(packets)):
-        if packets[i].seqnum == recvd_packet.seqnum:
-            packets[i] = None
-            send_base=i
-            windowsize = min(windowsize+1,MAXN)
+    if recvd_packet.seqnum == send_base:
+        packets[:send_base+1]=None
+        send_base+=1
+        send_base%=32
+        windowsize = min(windowsize+1,MAXN)
+    elif recvd_packet.seqnum == send_base-1 and dupcount<3:
+        dupcount+=1
+    elif dupcount==3:
+        clientSocket.sendto(packets[send_base].encode(),(emulator_addr, emulator_port))
+        windowsize = 1
+        dupcount=0
+        #restart timer
+
+
+
+
+        
 
 
 
